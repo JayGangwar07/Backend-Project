42e738d498e46106448f83274ab747442605f21a
JayGangwar07 <jaygangawar86@gmail.com>
2025-09-01 10:02:04 +0530

    Data limit tweak in app.js

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({limit: "20mb"}))
+app.use(express.urlencoded({extended: true, limit: "20mb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
@@ -38,3 +38,4 @@ app.use("/api/v1/dashboard", dashboardRouter)
 // http://localhost:8000/api/v1/users/register
 
 export { app }
+
